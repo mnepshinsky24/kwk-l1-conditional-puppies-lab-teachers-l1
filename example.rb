62b0c14b78b98dbda54f57451b53b528414668ff
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-07-11 15:29:12 +0000

    automatically backed up by learn

---
+++ example.rb
@@ -0,0 +1,14 @@
+
+def bank(my_bank_account_balance)
+if my_bank_account_balance > 10
+  "I'm going to eat steak tonight"
+  elsif my_bank_account_balance <= 10 && my_bank_account_balance > 5 
+  "I'm going to have lobster tonight"
+else 
+  "I'm going to eat cereal :("
+end 
+end
+
+puts bank(12)
+puts bank(10)
+puts bank(3)
--- puppies.rb
+++ puppies.rb
@@ -1,14 +1,52 @@
-# 1. Write a method called no_puppies that takes in a number of puppies as an argument. If the number of puppies is greater than zero then this method should return "No more puppies!" otherwise it should return "Well done, Rachel!"
+# # 1. Write a method called no_puppies that takes in a number of puppies as an argument. If the number of puppies is greater than zero then this method should return "No more puppies!" otherwise it should return "Well done, Rachel!"
 
+def no_puppies(number_of_puppies)
+  if number_of_puppies > 0 
+    return "No more puppies!"
+  else 
+    return "Good job, Rachel!"
+  end 
+end
 
+puts no_puppies(4)
+puts no_puppies(0)
 
-# 2. Rachel's animal accumulation has been going well and we're going to cut her some slack. Write a new method called less_puppies that takes in a number of puppies as an argument and tells Rachel good job when she gets less than 3 puppies, to slow down if she's found exactly 3 puppies, and to get back on her animal restriction if she gets more than 3 puppies.
+# # 2. Rachel's animal accumulation has been going well and we're going to cut her some slack. Write a new method called less_puppies that takes in a number of puppies as an argument and tells Rachel good job when she gets less than 3 puppies, to slow down if she's found exactly 3 puppies, and to get back on her animal restriction if she gets more than 3 puppies.
 
+def less_puppies(number_of_puppies)
+  if number_of_puppies < 3 
+        "Good job, Rachel!"
+      elsif number_of_puppies == 3
+        "Slow down, Rachel!"
+      else 
+        "Get back on your animal restriction, Rachel!"
+    end 
+  end
+  
+  puts less_puppies(10)
 
+# # # 3. We've decided to give Rachel a little more control over her puppy obsession. Write a method called some_puppies that takes in two arguments - the number of puppies she has found and a maximum puppy allowance. This method should tell Rachel good job if she gets less than half her maximum allowance and to get back on her puppy restriction when she exceeds her maximum.
 
-# 3. We've decided to give Rachel a little more control over her puppy obsession. Write a method called some_puppies that takes in two arguments - the number of puppies she has found and a maximum puppy allowance. This method should tell Rachel good job if she gets less than half her maximum allowance and to get back on her puppy restriction when she exceeds her maximum.
+def some_puppies(num_puppies, max)
+  if num_puppies < max/2
+    'Good job, Rachel!'
+  else
+    'Get back to your animal goals!'
+  end
+end
+puts some_puppies(20, 33)
 
+# # # BONUS
+# # 4. Rachel has decided to track both her puppy allowance AND her cat allowance. Write a method called new_animal accumulation that takes in two arguments - number of puppies and number of cats. As long as she gets zero puppies OR zero cats this method should return "Good job!". If she gets 1 or more puppies AND 1 or more cats it should tell her she should really slow back down!
 
+def new_animal(puppies, cats)
+  if puppies == 0 || cats == 0
+    'Good job!'
+  elsif puppies > 0 && cats > 0
+    'Get back to your animal goals!'
+  end
+end
 
-# BONUS
-# 4. Rachel has decided to track both her puppy allowance AND her cat allowance. Write a method called new_animal accumulation that takes in two arguments - number of puppies and number of cats. As long as she gets zero puppies OR zero cats this method should return "Good job!". If she gets 1 or more puppies AND 1 or more cats it should tell her she should really slow back down!
+puts new_animal(5,3)
+puts new_animal(0,0)
+puts new_animal(7,0)
